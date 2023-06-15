c0d88056cf0de53dd1782f8054502c0677f485d7
vpozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2023-06-15 17:38:54 +0300

    upd

--- ec23stress/gen_models.py
+++ ec23stress/gen_models.py
@@ -1,7 +1,7 @@
 import torch
 from torch.optim import Adam
 from pytorch_lightning import LightningModule
-from src.tcn_model import TCNModule
+from ec23stress.tcn_model import TCNModule
 
 class TCNGANModule(LightningModule):
     def __init__(
