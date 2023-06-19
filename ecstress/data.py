fe12a368ddc809892bc29ac29749375fb04afc2a
vpozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2023-06-19 17:05:36 +0300

    upd

--- ecstress/data.py
+++ ecstress/data.py
@@ -1,5 +1,7 @@
+import requests
 import torch
 from torch.utils.data import Dataset
+import pandas as pd
 
 class SlidingWindowDataset(Dataset):
     def __init__(self, ts, window_size, step_size):
@@ -14,3 +16,12 @@ class SlidingWindowDataset(Dataset):
         return torch.FloatTensor(
             self.ts[range(idx*self.step_size, idx*self.step_size + self.window_size)]
         )
+
+def get_ett_dataset():
+    r = requests.get(
+        'https://raw.githubusercontent.com/zhouhaoyi/ETDataset/main/ETT-small/ETTh1.csv'
+    )
+    open('ETTh1.csv', 'wb').write(r.content)
+    ett = pd.read_csv('ETTh1.csv', index_col=0)
+    ett.index = pd.to_datetime(ett.index)
+    return ett
